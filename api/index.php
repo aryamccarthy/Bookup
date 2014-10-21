e49eb8ef11d6748e6f14fbfd09d32fd5b4e27f4d
lukeoglesbee <lukewoglesbee@gmail.com>
2014-10-21 17:39:24 -0500

    API

---
+++ api/index.php
@@ -0,0 +1,53 @@
+<?php
+
+// Datbase information
+// Put your stuff here
+$host = 'localhost';
+$user = 'root';
+$pass = 'root';
+
+require 'vendor/autoload.php';
+
+// Get DB connection
+$app = new \Slim\Slim();
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=BurgerBar", "$user", "$pass");
+} catch (PDOException $e) {
+    $response = "Failed to connect: ";
+    $response .= $e->getMessage();
+    die ($response);
+}
+
+//
+//	FUNCTTIONS GO BELOW HERE
+//	Don't forget to import the global 
+//	VVVVVVVVVVVVVVVVVVVVVVV
+
+/**
+*   Hello
+*/
+$app->get('/hello/:last/:first/:MI', function($last, $first, $MI) {
+    echo "Hello, $first $MI. $last!";
+});
+$app->get('/hello', function() {
+	echo "Hello. I don't know your name.";
+});
+
+
+
+
+
+
+
+
+
+
+//	^^^^^^^^^^^^^^^^^^^^^^^
+//	FUNCTIONS GO ABOVE HERE
+//
+
+
+$app->run();
+
+
+?>
