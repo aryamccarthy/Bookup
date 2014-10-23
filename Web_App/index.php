19c8a1ca08a41d3c4266fed4aa6247e543b1369c
Marfle-Bark <eagleguy125@gmail.com>
2014-10-23 02:29:12 -0500

    Added a basic .gitignore and a placeholder index.php in Web_App.

---
+++ Web_App/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Test Page, Please Ignore</title>
+</head>
+<body>
+  <h1>TOTALLY 404'D</h1>
+  <h6>but totally not</h6>
+</body>
+</html>
