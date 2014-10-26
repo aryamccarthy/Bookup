a4e638084d6e09faaa2a85e91d4902306e2e6bb7
Abralincolnham <zfout@smu.edu>
2014-10-26 16:22:07 -0500

    altered isbn_num to a varchar type

--- Web_App/Database/MySQL/mysql_init.sql
+++ Web_App/Database/MySQL/mysql_init.sql
@@ -25,16 +25,16 @@ CREATE TABLE IF NOT EXISTS Account(
 DROP TABLE IF EXISTS BookList;
 
 CREATE TABLE IF NOT EXISTS BookList( 
-    isbn_num    BIGINT NOT NULL UNIQUE,
+    isbn_num    VARCHAR(15) NOT NULL UNIQUE,
     title       VARCHAR(45) NOT NULL,
-    author		VARCHAR(45) NOT NULL,
+    author	VARCHAR(45) NOT NULL,
     PRIMARY KEY(isbn_num)
 );
 
 DROP TABLE IF EXISTS PopularBook;
 
 CREATE TABLE IF NOT EXISTS PopularBook(
-    isbn_num    BIGINT NOT NULL UNIQUE,
+    isbn_num    VARCHAR(15) NOT NULL UNIQUE,
     PRIMARY KEY(isbn_num),
     FOREIGN KEY(isbn_num) REFERENCES  BookList(isbn_num)
 );
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS Rating(
     account_id  INT NOT NULL UNIQUE,
     rating      INT,
     timestamp   DATETIME,
-    isbn_num    BIGINT NOT NULL UNIQUE,
+    isbn_num    VARCHAR(15) NOT NULL UNIQUE,
     PRIMARY KEY(account_id),
     FOREIGN KEY(account_id) REFERENCES Account(account_id),
     FOREIGN KEY(isbn_num) REFERENCES BookList(isbn_num)
@@ -56,7 +56,7 @@ DROP TABLE IF EXISTS ReadingList;
 CREATE TABLE IF NOT EXISTS ReadingList(
     account_id  INT NOT NULL UNIQUE,
     timestamp   DATETIME,
-    isbn_num    BIGINT NOT NULL UNIQUE,
+    isbn_num    VARCHAR(15) NOT NULL UNIQUE,
     PRIMARY KEY(account_id),
     FOREIGN KEY(account_id) REFERENCES Account(account_id),
     FOREIGN KEY(isbn_num) REFERENCES BookList(isbn_num)
@@ -68,7 +68,7 @@ CREATE TABLE IF NOT EXISTS BookSeen(
     account_id  INT NOT NULL UNIQUE,
     rating      INT,
     timestamp   DATETIME,
-    isbn_num    BIGINT NOT NULL UNIQUE,
+    isbn_num    VARCHAR(15) NOT NULL UNIQUE,
     PRIMARY KEY(account_id),
     FOREIGN KEY(account_id) REFERENCES Account(account_id),
     FOREIGN KEY(isbn_num) REFERENCES BookList(isbn_num)
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS BookSeen(
 DROP TABLE IF EXISTS BookHash;
 
 CREATE TABLE IF NOT EXISTS BookHash(
-    isbn_num    BIGINT NOT NULL UNIQUE,
+    isbn_num    VARCHAR(15) NOT NULL UNIQUE,
     hash_val    INT,
     PRIMARY KEY(hash_val),
     FOREIGN KEY(isbn_num) REFERENCES BookList(isbn_num)
