49517c73f2c76a762aa703d7e146aadf0c1711a4
drizzuto <drizzuto@smu.edu>
2014-10-28 12:33:06 -0500

    reverted addToFirebase.py

--- Web_App/Database/Firebase/addToFirebase.py
+++ Web_App/Database/Firebase/addToFirebase.py
@@ -1,18 +1,9 @@
-# Title: addToFirebase.py
-# Summary: python script for querying google books api
-# Owner: Danny Rizzuto
-# Version: 1.1
-# Last Modified: 10/27/2014
-# Last Modified By: Zack Fout
-# Notes: added functionality to query google books api by title
-
 from firebase import firebase
 import requests
 
-fbURL = 'https://blistering-torch-3821.firebaseio.com/'
-booksURL = 'https://www.googleapis.com/books/v1/volumes'
-
 def readFile():
+
+    openFile = ("5000isbn.txt", "rw+")
 
     filePath = '';
 
@@ -20,31 +11,33 @@ def readFile():
 
     return isbnArray
 
-def parseQuery(query): # replaces whitespaces with html encoding
-    if " " in query:
-        separator = "%20"
-        queryArr = query.split(' ')
-        return separator.join(queryArr)
-    else:
-        return query
+def addToFirebase(isbnArray):
 
-def addByIsbn(isbnArray): # queries by isbn number and inserts into firebase
+
+    fbURL = 'https://blistering-torch-3821.firebaseio.com/'
+
     for isbn in isbnArray:
-        googleQuery = googleURL + str(isbn)
-        googleRequest = requests.get(googleQuery);
-        firebaseConn = firebase.FirebaseApplication(fbURL, None)
-        firebaseConn.post(str(isbn), googleRequest.content)
 
-def addByTitle(title): # queries by title and inserts into firebase
-   normalizedQuery = parseQuery(title)
-   queryTerm = "?q=" + normalizedQuery
-   googleQuery = booksURL + queryTerm
-   googleRequest = requests.get(googleQuery)
-   fbConnection = firebase.FirebaseApplication(fbURL, None)
-   fbConnection.post(str(title), googleRequest.content)
+        googleQuery = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + str(isbn)
+
+        print googleQuery;
+
+        googleRequest = requests.get(googleQuery);
+
+        #print googleRequest.content
+
+        firebaseConn = firebase.FirebaseApplication(fbURL, None)
+
+        firebaseResult = firebaseConn.post(str(isbn), googleRequest.content)
 
 if __name__ == "__main__":
-    #addByTitle("Fahrenheit 451")
+
+    # isbnArray = readFile()
+
+    isbnArray = ['9780141182957']
+
+
+    addToFirebase(isbnArray)
 
 
 
