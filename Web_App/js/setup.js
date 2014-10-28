0172ef11a4426a3e4ebd226d8c5b1183f2fce000
Katherine Habeck <khabeck@smu.edu>
2014-10-28 01:02:34 -0500

    Small test in retrieving data from json

--- Web_App/js/setup.js
+++ Web_App/js/setup.js
@@ -7,13 +7,23 @@ getPopularBooks();
 var rootURL= "http://localhost:8888/api/index.php";
 
 function getPopularBooks() {
+
 	$.ajax({
 		type: 'GET',
 		url: rootURL + "/getPopularBooks",
 		dataType: "json",
 		success: function (data) {
-			console.log(data);
-			alert(JSON.stringify(data));	
+
+		var bookObjs = data.PopularBooks;
+
+
+			for(var i=0; i<bookObjs.length; i++){
+				var parsedBooks = $.parseJSON(bookObjs[i]);
+				var title= parsedBooks.items[0].volumeInfo.title;
+				var author =parsedBooks.items[0].volumeInfo.authors;
+				var thumbnail=parsedBooks.items[0].volumeInfo.imageLinks.thumbnail;
+				console.log(thumbnail);
+			}
 	}	
 	});
 }
