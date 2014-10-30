048d795b9a2c1f6a49f5726458ba89bbae4e010a
Marfle-Bark <eagleguy125@gmail.com>
2014-10-30 16:45:28 -0500

    Quick update to refactor some login-specific styles into login.css and merged a few duplicate CSS selectors.

--- Web_App/login.php
+++ Web_App/login.php
@@ -17,23 +17,23 @@
 
     <?php include("login/err.php"); ?>
 
-    <form action="index.php" method="post" id="loginform" name="loginform">
+    <form action="index.php" method="post" id="login" name="loginform">
       <ul>
         <li>Login/Sign Up</li>
         <li><hr></li>
         <li>
-          <label for="login_input_email">Email</label>
-          <input id="login_input_email" name="user_email" type="email" pattern=".{3,30}" placeholder="someone@example.com" autofocus required>
+          <label for="email">Email</label>
+          <input id="email" name="user_email" type="email" pattern=".{3,30}" placeholder="someone@example.com" autofocus required>
         </li>
 
         <li>
-          <label for="login_input_password">Password</label>
-          <input id="login_input_password" name="user_password" type="password" pattern = ".{7,30}" placeholder="••••••••••••" required>
+          <label for="password">Password</label>
+          <input id="password" name="user_password" type="password" pattern = ".{7,30}" placeholder="••••••••••••" required>
         </li>
 
         <li>
-          <button id="register_user" class="twobutton">Sign Up</button>
-          <input id="login_user" type="submit" name="login" class="twobutton" value="Login"/>
+          <button id="register" class="twobutton">Sign Up</button>
+          <input id="login_button" type="submit" name="login" class="twobutton" value="Login"/>
         </li>
       </ul>
 
