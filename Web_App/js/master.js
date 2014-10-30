caf07dedcb1da0d6f80e709141861a4bb02a4a13
Katherine Habeck <khabeck@smu.edu>
2014-10-29 23:07:03 -0500

    Successfully generates html for a given book object

--- Web_App/js/master.js
+++ Web_App/js/master.js
@@ -1,9 +1,7 @@
 $(document).ready( function() {
 
-	getRandomBook();
-
+	bookObjTest();
 	for( var i=0; i<discoveryBooks.length; i++){
-		//TODO: generate html for each book in setup array
 	}
 
 }); 
@@ -13,11 +11,12 @@ var rootURL= "http://localhost:8888/api/index.php";
 var discoveryBooks = [];
 var readingList = [];
 
-function Book( title, author, description, cover){
+
+function Book( title, author, cover, description){
 	this. title=title;
 	this.author=author;
 	this.description=description;
-	this.thumbnail=thumbnail;
+	this.cover=cover;
 }
 
 //TODO: untested, waiting on completed api method
@@ -48,6 +47,18 @@ function getRandomBook() {
 	});
 }
 
+//TODO: implement and test
+function getReadingList() {
+
+	$.ajax({
+		type: 'GET',
+		url: rootURL + "/getReadingList",
+		dataType: "json",
+		success: function (data) {
+			
+		}	
+	});
+} 
 
 //TODO: implement and test
 function addBookToReadingList() {
@@ -62,18 +73,6 @@ function addBookToReadingList() {
 	});
 }
 
-//TODO: implement and test
-function getReadingList() {
-
-	$.ajax({
-		type: 'GET',
-		url: rootURL + "/getReadingList",
-		dataType: "json",
-		success: function (data) {
-			
-		}	
-	});
-} 
 
 //TODO: implement and test
 function submitBookFeedback() {
@@ -105,3 +104,77 @@ function overlay() {
   var el = document.getElementById("rate_from_discovery");
   el.style.visibility = (el.style.visibility === "visible") ? "hidden" : "visible";
 }
+
+
+var rootURL= "http://localhost:8888/api/index.php";
+
+var booksForSetup = [];
+
+function getPopularBooks() {
+
+	$.ajax({
+		type: 'GET',
+		url: rootURL + "/getPopularBooks",
+		dataType: "json",
+		success: function (data) {
+			var bookObjs = data.PopularBooks;
+			alert(data);
+
+			for(var i=0; i<bookObjs.length; i++){
+				var parsedBooks = $.parseJSON(bookObjs[i]);				
+
+				var title= parsedBooks.items[0].volumeInfo.title;
+				var author =parsedBooks.items[0].volumeInfo.authors;
+				var thumbnail=parsedBooks.items[0].volumeInfo.imageLinks.thumbnail;
+				
+				var cover = new Image();
+				cover.src = thumbnail.imgPath;
+				var newBook= new Book(title, author, cover);
+			 	booksForSetup.push(newBook);
+			}
+		}	
+	});
+}
+
+function bookObjTest() {
+
+	$.ajax({
+		type: 'GET',
+		url: rootURL + "/getBookFromFirebase?isbn=9780001000391",
+		dataType: "json",
+		success: function (data) {
+			//var bookObjs = data.PopularBooks;
+			//for(var i=0; i<bookObjs.length; i++){
+				var parsedBooks = $.parseJSON(data);
+
+				var title= parsedBooks.items[0].volumeInfo.title;
+				var author =parsedBooks.items[0].volumeInfo.authors[0];
+				var thumbnail=parsedBooks.items[0].volumeInfo.imageLinks.thumbnail;
+				var description =parsedBooks.items[0].volumeInfo.description;
+				var cover = new Image();
+				cover.src = thumbnail;
+				var newBook= new Book(title, author, cover, description);
+			 	generateHTML(newBook);
+		
+		}	
+	});
+
+}
+
+function generateHTML(Book){
+	var account_section = document.getElementById("account_setup");
+
+	account_section.appendChild(Book.cover);
+
+	var title = document.createElement("figcaption");
+	title.innerHTML=Book.title;
+	account_section.appendChild(title);
+
+	var author = document.createElement("figcaption");
+	author.innerHTML=Book.author;
+	account_section.appendChild(author);
+
+
+}
+
+
--- Web_App/setup.php
+++ Web_App/setup.php
@@ -5,7 +5,6 @@
   
   <!-- Page-specific includes -->
   <link rel="stylesheet" href="css/login.css">
-  <script type="text/javascript" src="js/stylesheet.js"></script>
 
   <title>Bookup</title>
 </head>
