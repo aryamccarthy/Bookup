09260460f75725167a4148807a02e915819c6899
Marfle-Bark <ethan.busbee@gmail.com>
2014-11-01 19:59:52 -0500

    Cleaned up login folder.

--- Web_App/login/includes.php
+++
@@ -1,4 +0,0 @@
-<?php
-  require_once("config/db.php");  //localhost/BookUp/root/root
-  require_once("classes/Login.php");
-  ?>
--- Web_App/sesh.php
+++ Web_App/sesh.php
@@ -4,8 +4,8 @@ mb_internal_encoding('UTF-8');
 mb_http_input('UTF-8');
 mb_http_output('UTF-8');
 
-require_once("login/config/db.php");
-require_once("login/classes/Login.php");
+require_once("login/db.php");
+require_once("login/Login.php");
 
 $login = new Login();
 if(!($login->isUserLoggedIn()))                 //if user is not logged in
