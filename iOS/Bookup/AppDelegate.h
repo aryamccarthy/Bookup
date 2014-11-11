a2ec94d48a5dde17b7f4addaa3f7eacce1e053c1
aryamccarthy <arya.mccarthy@gmail.com>
2014-11-11 14:51:56 -0600

    Initial iOS commit

---
+++ iOS/Bookup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bookup
+//
+//  Created by Arya McCarthy on 11/11/14.
+//  Copyright (c) 2014 Arya McCarthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/Bookup/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bookup
+//
+//  Created by Arya McCarthy on 11/11/14.
+//  Copyright (c) 2014 Arya McCarthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
