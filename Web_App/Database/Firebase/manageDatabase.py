bd81bf88b3a1d91942447ec1cc43918497b1c2a5
drizzuto <drizzuto@smu.edu>
2014-11-14 13:20:43 -0600

    working on strict book object contraints

--- Web_App/Database/Firebase/manageDatabase.py
+++ Web_App/Database/Firebase/manageDatabase.py
@@ -39,61 +39,80 @@ def connectToBookUp():
         print "Could not connect to database, got error:\n %s" % (e.args[1])
         sys.exit(1)
 
-def addToDatabase(isbnArray, db, mode):
+def addToDatabase(isbnArray, db):
+
+    """
+    :param isbnArray: array of isbns being added to database
+    :param db: a connection to the database
+    :return: no return
+
+    This function accepts a connection to a database and an array of isbns and adds them to the database
+    The google API call will be tested for a Title, Author, Description, and Image Link
+
+    The language will also be taken down for possible future multi-langual versions
+    """
+
+    fbURL = 'https://bookup-v3.firebaseio.com/'
 
     cursor = db.cursor()
 
     for isbn in isbnArray:
 
-        try:
+        googleQuery = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + str(isbn)
 
-            cursor.execute("INSERT INTO BookList VALUES (%s)", (isbn))
+        googleRequest = requests.get(googleQuery)
+
+        # print googleRequest.content
+
+        jsontest = json.loads(googleRequest.content)
+
+        #print jsontest
+
+        if "error" not in jsontest:
+
+            if jsontest['totalItems'] is not 0:
+
+                pass
+
+                # print "json was not an error"
+
+                # firebaseConn = firebase.FirebaseApplication(fbURL, None)
+                #
+                # firebaseResult = firebaseConn.post(str(isbn), googleRequest.content)
+                #
+                # with open("Isbn_Txt_Files/list_of_Isbn_in_FB.txt", "a") as myfile:
+                #     textString = "{}\n".format(isbn)
+                #     myfile.write(textString)
+            else:
+
+                cursor.execute("INSERT INTO BookList_Bad VALUES (%s, %s)", ("Bad API Call"))
+
+                db.commit()
+
+                print "\t" + isbn + " not added bc No Items"
+
+
+        else:
+
+            cursor.execute("INSERT INTO BookList_Bad VALUES (%s, %s)", ("Bad API Call"))
 
             db.commit()
 
-            print isbn + " added to database"
-
-        except MySQLdb.Error, e:
-
-            print "\t" + isbn + " not added to database"
+            print "\t" + isbn + " not added bc Bad API Call"
 
 
-    # fbURL = 'https://bookup-v3.firebaseio.com/'
+        # try:
+        #
+        #     cursor.execute("INSERT INTO BookList VALUES (%s)", (isbn))
+        #
+        #     db.commit()
+        #
+        #     print isbn + " added to database"
+        #
+        # except MySQLdb.Error, e:
+        #
+        #     print "\t" + isbn + " not added to database"
 
-    # for isbn in isbnArray:
-    #
-    #     googleQuery = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + str(isbn)
-    #
-    #     googleRequest = requests.get(googleQuery)
-    #
-    #     # print googleRequest.content
-    #
-    #     jsontest = json.loads(googleRequest.content)
-    #
-    #     #print jsontest
-    #
-    #     if "error" not in jsontest:
-    #
-    #         if jsontest['totalItems'] is not 0:
-    #
-    #             # print "json was not an error"
-    #
-    #             firebaseConn = firebase.FirebaseApplication(fbURL, None)
-    #
-    #             firebaseResult = firebaseConn.post(str(isbn), googleRequest.content)
-    #
-    #             with open("Isbn_Txt_Files/list_of_Isbn_in_FB.txt", "a") as myfile:
-    #                 textString = "{}\n".format(isbn)
-    #                 myfile.write(textString)
-    #         else:
-    #
-    #             print isbn + " not added"
-    #             print "There were no Items"
-    #
-    #     else:
-    #
-    #         print isbn + " not added"
-    #         print "json was an error"
 
 def cleanBookList_Bad(db):
 
@@ -127,7 +146,7 @@ if __name__ == "__main__":
     db = connectToBookUp()
 
     isbnArray = ['9781904233657']
-    addToDatabase(isbnArray, db, 0)
+    addToDatabase(isbnArray, db)
 
     db.close()
 
