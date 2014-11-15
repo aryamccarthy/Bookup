c1ef8e5f7ffa38c856ae1c243002f0dcee00c197
kaurorah <khabeck@smu.edu>
2014-11-15 15:53:47 -0600

    Added a login/logout system

---
+++ iOS/Bookup/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Bookup
+//
+//  Created by Katherine Habeck on 11/15/14.
+//  Copyright (c) 2014 Arya McCarthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
